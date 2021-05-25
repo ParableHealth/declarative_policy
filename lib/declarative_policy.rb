46d0156401c4258f0a7d7ee589d5bec5d14925a0
Alex Kalderimis <akalderimis@gitlab.com>
2021-05-25 13:36:15 +0100

    Add missing require set

--- lib/declarative_policy.rb
+++ lib/declarative_policy.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'set'
 require_relative 'declarative_policy/cache'
 require_relative 'declarative_policy/condition'
 require_relative 'declarative_policy/delegate_dsl'
