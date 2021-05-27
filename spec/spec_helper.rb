9ca90e68e21c8417feaa2153e190672decc7cea7
Alex Kalderimis <akalderimis@gitlab.com>
2021-05-27 12:02:55 +0100

    Conditional include for byebug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
-require 'pry-byebug'
+require 'pry-byebug' if RUBY_ENGINE != 'jruby'
 require 'rspec-parameterized'
 
 require 'declarative_policy'
